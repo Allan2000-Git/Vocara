c345239554f6c35b130efc2ad7a84c421f2d0049
Allan Joston Fernandes <allanfernandes459@gmail.com>
2024-06-09 23:26:27 +0530

    create types and vocara ai model

---
+++ types/types.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const formSchema = z.object({
+    jobRole: z.string().min(3, {message: "Must have minimum of 3 characters"}).max(50),
+    jobDescription: z.string().min(3, {message: "Must have minimum of 3 characters"}).max(500),
+    jobExperience: z.coerce.number().int().positive().min(1, {message: "Add minimum experience to job your interviewing"}),
+    mockResponse: z.any()
+});
+
+export type JobSchemaType = z.infer<typeof formSchema>;
