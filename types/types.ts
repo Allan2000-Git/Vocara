93fc54d0e9c7170bd58a79f2bf6007502a4324dd
Allan Joston Fernandes <allanfernandes459@gmail.com>
2024-06-10 23:28:19 +0530

    fix: minor change in types

--- types/types.ts
+++ types/types.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const formSchema = z.object({
     jobRole: z.string().min(3, {message: "Must have minimum of 3 characters"}).max(50),
     jobDescription: z.string().min(3, {message: "Must have minimum of 3 characters"}).max(500),
-    jobExperience: z.coerce.number().int().positive().min(1, {message: "Add minimum experience to job your interviewing"}),
+    jobExperience: z.string().min(1, {message: "Add minimum experience to job your interviewing"}),
     mockResponse: z.any()
 });
 
