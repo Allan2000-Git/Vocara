fd6587c2e89b6694d4f2d25294aabd77551600ea
Allan Joston Fernandes <allanfernandes459@gmail.com>
2024-06-11 23:34:10 +0530

    fix navbar and add logo

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,10 +1,32 @@
 "use client";
 
 import React, { useState } from "react";
-import { HoveredLink, Menu, MenuItem, ProductItem } from  "@/components/ui/navbar-menu";
+import { Menu, MenuItem } from  "@/components/ui/navbar-menu";
 import { cn } from "@/lib/utils";
-import { SignedIn, SignInButton, UserButton } from "@clerk/nextjs";
+import { SignedIn, SignInButton, UserButton, useSession } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Logo from "./Logo";
+import { Skeleton } from "@/components/ui/skeleton";
+
+const links = [
+    {
+        path: "/dashboard",
+        label: "Dashboard"
+    },
+    {
+        path: "/questions",
+        label: "Questions"
+    },
+    {
+        path: "/pricing",
+        label: "Pricing"
+    },
+    {
+        path: "/how-it-works",
+        label: "How it works"
+    }
+]
 
 export function NavbarDemo() {
     return (
@@ -16,29 +38,44 @@ export function NavbarDemo() {
 
 function Navbar({ className }: { className?: string }) {
     const [active, setActive] = useState<string | null>(null);
+    const { isLoaded, session, isSignedIn } = useSession();
+
     return (
         <div
-        className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50 ", className)}
+        className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
         >
         <Menu setActive={setActive}>
-            <div className="flex items-center justify-between w-full">
-            <div className="text-black dark:text-white">
-                    Logo
-                </div>
+            <div className="flex items-center justify-between w-full text-sm">
+                <Logo />
                 <div className="flex gap-5">
-                    <MenuItem setActive={setActive} active={active} item="Dashboard" />
-                    <MenuItem setActive={setActive} active={active} item="Products" />
-                    <MenuItem setActive={setActive} active={active} item="Pricing" />
+                    {
+                        links.map((link, index) => (
+                            <Link key={index} href={link.path}>
+                                <MenuItem setActive={setActive} active={active} item={link.label} />
+                            </Link>
+                        ))
+                    }
                 </div>
-                <div className="text-black dark:text-white">
-                    <SignedIn>
-                        <UserButton />
-                    </SignedIn>
-                    <SignInButton>
-                        <Button className="w-[100px] h-[45px] rounded-full">
-                            Sign in
-                        </Button>
-                    </SignInButton>
+                <div className="flex items-center justify-center">
+                    {
+                        isSignedIn ? (
+                            <>
+                                {
+                                    isLoaded ? (
+                                        <UserButton />
+                                    ):(
+                                        <Skeleton className="h-8 w-8 rounded-full" />
+                                    )
+                                }
+                            </>
+                        ) : (
+                            <SignInButton forceRedirectUrl={"/dashboard"}>
+                                <Button className="w-[100px] h-[42px] rounded-full text-sm">
+                                    Sign in
+                                </Button>
+                            </SignInButton>
+                        )
+                    }
                 </div>
             </div>
         </Menu>
