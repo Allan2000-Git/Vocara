0b9784916793f505a21eae3ccea2a193a5182ab2
Allan Joston Fernandes <allanfernandes459@gmail.com>
2024-06-16 16:34:04 +0530

    create server action to store the user answer in db

--- actions/interview.ts
+++ actions/interview.ts
@@ -1,8 +1,8 @@
 "use server";
 
 import { db } from "@/app/db";
-import { interviews } from "@/app/db/schema";
-import { InterviewDetails, JobSchemaType } from "@/types/types";
+import { answers, interviews } from "@/app/db/schema";
+import { AnswerType, InterviewDetails, JobSchemaType } from "@/types/types";
 import { currentUser } from "@clerk/nextjs/server";
 import { v4 as uuidv4 } from 'uuid';
 import moment from 'moment';
@@ -49,5 +49,30 @@ export async function getInterviewDetailsById(mockId: string) {
         return interview[0] as InterviewDetails;
     } catch (error) {
         throw new Error('Something went wrong while getting the details.')
+    }
+}
+
+export async function storeAnswer(values: AnswerType) {
+    try {
+        const user = await currentUser();
+        if (!user) {
+            throw new Error("You must be logged in to start an interview.");
+        }
+        console.log(values);
+        const newAnswer = await db.insert(answers).values({
+            userId: user.id,
+            mockId: values.mockId,
+            question: values.question,
+            possibleAnswer: values.possibleAnswer,
+            userAnswer: values.userAnswer,
+            feedback: values.feedback,
+            overall_rating: values.overall_rating,
+            createdAt: moment().format('MMMM Do YYYY, h:mm:ss a')
+        });
+
+        return newAnswer;
+    } catch (error) {
+        console.log(error);
+        return error;
     }
 }
